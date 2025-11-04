984929998870774bef203646af88c65a249ea6af
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-11-04 16:51:12 +0530

    Update bot.py

--- TNC/core/bot.py
+++ TNC/core/bot.py
@@ -1,29 +1,18 @@
-# TNC/core/bot.py
-
-from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus
 import sys
 
-# config.py is no longer imported
+import config
 from ..logging import LOGGER
 
 
-# ---!! FILL YOUR VALUES HERE !! ---
-MY_API_ID = 24727770  # <-- Put your API_ID here
-MY_API_HASH = "b29e54a12450d2bf91e23b5d90d5378e"  # <-- Put your API_HASH here
-MY_BOT_TOKEN = "8469011170:AAGfldtzgZV1qS-YxKs35We7X1KZiH3K8NU"  # <-- Put your BOT_TOKEN here
-MY_LOGGER_ID = -1003128590255  # <-- Put your LOGGER_ID here
-# ------------------------------------
-
-
 class TNC(Client):
-    def __init__(self, name="AlyaXRobot"):
+    def __init__(self, name="ZoyuXmusicRobot"):
         LOGGER(__name__).info("Starting Bot...")
         super().__init__(
             name=name,
-            api_id=MY_API_ID,  # <-- Using hardcoded value
-            api_hash=MY_API_HASH,  # <-- Using hardcoded value
-            bot_token=MY_BOT_TOKEN,  # <-- Using hardcoded value
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
             in_memory=True,
             max_concurrent_transmissions=7,
             parse_mode="html",
@@ -39,7 +28,7 @@ class TNC(Client):
 
         try:
             await self.send_message(
-                chat_id=MY_LOGGER_ID,  # <-- Using hardcoded value
+                chat_id=config.LOGGER_ID,
                 text=(
                     f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b></u>\n\n"
                     f"ɪᴅ : <code>{self.id}</code>\n"
@@ -58,7 +47,7 @@ class TNC(Client):
             )
             sys.exit(1)
 
-        member = await self.get_chat_member(MY_LOGGER_ID, self.id)  # <-- Using hardcoded value
+        member = await self.get_chat_member(config.LOGGER_ID, self.id)
         if member.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
